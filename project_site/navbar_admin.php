cdc8c096fe3950580fbfd352a65f4b12de95c42e
Tom-Brian Garcia <tbg-le-nolife@hotmail.fr>
2017-10-06 17:28:57 +0200

    3 different navbar

---
+++ project_site/navbar_admin.php
@@ -0,0 +1,10 @@
+<!-- ------ "Navbar" : Menu de Navigation ------ -->
+<div class="collapse menu" id="menu">
+    <ul class="list-inline">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="management.php">Management</a></li>
+        <li><a href="login.php">Logout</a></li>
+    </ul>
+</div>
+<!-- ------ "Navbar" : Fin du Bloc ------ -->
