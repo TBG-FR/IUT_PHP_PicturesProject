78925d0211e543c8de668eff3708657759323e86
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2017-10-12 09:51:54 +0200

    PHP : Database class, tests on index.php
    Added "database" class (from TP3) and some tests

--- project_site/classes/conf.bdd.php
+++ project_site/classes/conf.bdd.php
@@ -1,1 +1,18 @@
+
+<?php
+
 // For security reasons, rename this file with a "weird" (unexpected) name, making it harder to find for potential "hackers"
+// add global ?
+/* IUT
+$host="localhost";
+$user="p1520325";
+$password="11520325";
+$name="p1520325";*/
+/* Wamp
+$host="localhost";
+$user="root";
+$password="";
+$name="bdd";*/
+
+// TO_TRANSLATE
+// on évitera de fermer la balise php pour ne pas injecter de caratères invible sur les pages parentes
