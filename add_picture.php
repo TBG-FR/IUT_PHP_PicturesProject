f305e28350819188c3bea120d0f2f322fbfc8aeb
Dyvia <dyviafleury@gmail.com>
2017-10-12 12:29:39 +0200

    PHP : add pictures

---
+++ add_picture.php
@@ -0,0 +1,13 @@
+<html>
+   <head>
+      <title>Stock d'images</title>
+   </head>
+   <body>
+      <h3>Envoi d'une image</h3>
+      <form enctype="multipart/form-data" action="upload_image.php" method="post">
+         <input type="text" name="pic_name" />
+         <input type="file" name="pic" />
+         <input type="submit" value="Envoyer" />
+      </form>
+   </body>
+</html>
