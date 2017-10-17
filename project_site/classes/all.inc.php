4525fe03ee9d703f9840f1e47ad947aee3f1ddb6
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2017-10-17 15:42:49 +0200

    Includes : Classes
    Added Classes User & Database includes

--- project_site/classes/all.inc.php
+++ project_site/classes/all.inc.php
@@ -2,7 +2,8 @@
 
     include_once("various.inc.php"); // COMMENT
     include_once("conf.bdd.php"); // COMMENT
-    include_once("database.php"); // COMMENT
+    include_once("Database.php"); // COMMENT
+    include_once("User.php"); // COMMENT
     //include_once("example.inc.php"); // COMMENT
     //include_once("imgwatermark.inc.php"); // COMMENT
 
