db6be0a8ea879da8427aca8c8179bdddd43f6ebf
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-10-19 23:18:30 +0200

    PHP - Test : Database Inserts
    Looks like Database::save() doesn't work well...
    Made some tests here, could be useful in the future, maybe

---
+++ project_site/test.php
@@ -0,0 +1,63 @@
+<?php
+
+    require_once("classes/all.inc.php"); // Include all the Classes & Functions & Co + Session Start
+
+
+                
+                $db = new Database();
+
+    echo "JUSTE BEFORE SAVE // JUSTE BEFORE SAVE // JUSTE BEFORE SAVE // JUSTE BEFORE SAVE";
+
+$var1 = "ALEzzzEEELO";
+    $var2 = "ALzzEEEEEEEUIL";
+$var12 = "phpproj_user";
+            
+            $r1 = $db->query("INSERT INTO $var12 (username, password, admin) VALUES ('$var1', '$var2', '0')",array(),TRUE);
+
+
+//$r1 = $db->save("phpproj_user", array(
+//    'username' => 'ABJ',
+//    'admin' => 0,
+//    'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+//));
+
+
+
+            $r2 = $db->query("SELECT * FROM phpproj_user");
+            
+//            $red = $db->save("users", array(
+//            'nom' => 'Martin',
+//            'prenom' => 'Thomas',
+//            'age' => 34,
+//            'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+//            ));
+//            
+//            $up = $db->save("phpproj_user", array(
+//            'username' => $login,
+//            'password' => $pass, // Password will be automatically crypted using hash() method from Database class
+//            //'firstname' => $f_name,
+//            //'lastname' => $l_name,
+//            'admin' => 0
+//            ));
+//            
+//            $res = $db->save("phpproj_user", array(
+//                'username' => 'Martin',
+//                'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+//            ));
+//            
+//            $rev = $db->save("users", array(
+//                'id' => 145,
+//                'nom' => 'Martin',
+//                'prenom' => 'Thomas',
+//                'age' => 35,
+//                'password' => 'monAutreSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+//            ));
+//            
+//            /* TEMP */ var_dump($red);
+//            /* TEMP */ var_dump($up);
+//            /* TEMP */ var_dump($res);
+//            /* TEMP */ var_dump($rev);
+            /* TEMP */ var_dump($r1);
+            /* TEMP */ var_dump($r2);
+
+?>
