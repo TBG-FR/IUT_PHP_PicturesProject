90d76df1c98e36543a7dacb26bc72ae0b2157367
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-10-30 12:33:24 +0100

    PHP Login - Comments about Disconnection
    Modified some comments

--- project_site/test.php
+++ project_site/test.php
@@ -11,48 +11,52 @@
 $var1 = "ALEzzzEEELO";
     $var2 = "ALzzEEEEEEEUIL";
 $var12 = "phpproj_user";
-            
+//            
             $r1 = $db->query("INSERT INTO $var12 (username, password, admin) VALUES ('$var1', '$var2', '0')",array(),TRUE);
 
 
-//$r1 = $db->save("phpproj_user", array(
-//    'username' => 'ABJ',
-//    'admin' => 0,
-//    'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
-//));
+$r1 = $db->save("phpproj_user", array(
+    'username' => 'ABJ',
+    'admin' => 0,
+    'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+));
 
 
 
-            $r2 = $db->query("SELECT * FROM phpproj_user");
             
-//            $red = $db->save("users", array(
-//            'nom' => 'Martin',
-//            'prenom' => 'Thomas',
-//            'age' => 34,
-//            'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
-//            ));
+            $red = $db->save("users", array(
+            'nom' => 'AAMartin',
+            'prenom' => 'Thomas',
+            'age' => 34,
+            'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+            ));
+
+            $login = bite;
+            $pass = loge;
+            
+            $up = $db->save("phpproj_user", array(
+            'username' => $login,
+            'password' => $pass, // Password will be automatically crypted using hash() method from Database class
+            //'firstname' => $f_name,
+            //'lastname' => $l_name,
+            'admin' => 0
+            ));
+
+            $res = $db->save("phpproj_user", array(
+                'username' => 'BBMartin',
+                'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+            ));
+            
+            $rev = $db->save("users", array(
+                'id' => 145,
+                'nom' => 'CCMartin',
+                'prenom' => 'Thomas',
+                'age' => 35,
+                'password' => 'monAutreSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
+            ));
 //            
-//            $up = $db->save("phpproj_user", array(
-//            'username' => $login,
-//            'password' => $pass, // Password will be automatically crypted using hash() method from Database class
-//            //'firstname' => $f_name,
-//            //'lastname' => $l_name,
-//            'admin' => 0
-//            ));
-//            
-//            $res = $db->save("phpproj_user", array(
-//                'username' => 'Martin',
-//                'password' => 'monSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
-//            ));
-//            
-//            $rev = $db->save("users", array(
-//                'id' => 145,
-//                'nom' => 'Martin',
-//                'prenom' => 'Thomas',
-//                'age' => 35,
-//                'password' => 'monAutreSuperMotDePasse' // le mot de passe sera crypté automatiquement selon la règle choisi dans la méthode hash()
-//            ));
-//            
+            $r2 = $db->query("SELECT * FROM phpproj_user");
+
 //            /* TEMP */ var_dump($red);
 //            /* TEMP */ var_dump($up);
 //            /* TEMP */ var_dump($res);
