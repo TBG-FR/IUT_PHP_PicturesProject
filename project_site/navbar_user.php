0583edeab8f7de55d0cfde216946f8324c2805d9
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-10-30 13:32:37 +0100

    PHP Navbar : Account link
    Modified the link under "Account" button (will bring the user on login.php, displaying the submenus

--- project_site/navbar_user.php
+++ project_site/navbar_user.php
@@ -3,8 +3,8 @@
     <ul class="list-inline">
         <li><a href="index.php">Home</a></li>
         <li><a href="gallery.php">Gallery</a></li>
-        <li><a href="account.php">Account</a></li>
-        <li><a href="cart.php">Cart (0)</a></li>
+        <li><a href="login.php">Account</a></li>
+        <li><a href="cart.php">Cart (?)</a></li>
         <li><a href="login.php?action=disconnect&source=<?php echo basename($_SERVER['PHP_SELF']); ?>">Logout</a></li>
     </ul>
 </div>
