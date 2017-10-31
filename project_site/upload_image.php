cc9a4617475640ce929758ab1824dc56ae7b5d0b
Dyvia <dyviafleury@gmail.com>
2017-11-01 00:38:40 +0100

    ADD FORM INFO INTO PIC CLASS

--- project_site/upload_image.php
+++ project_site/upload_image.php
@@ -1,4 +1,3 @@
-
 <?php
 
 require_once("classes/all.inc.php");
@@ -8,12 +7,18 @@ $pic= new Picture();
 if($pic->addImage($_FILES['pic']['name'],$_FILES['pic']['error'],$_FILES['pic']['tmp_name'])) {
     
     $img="public_images/{$_FILES['pic']['name']}";
+    
     echo "<img src='$img' alt='texte alternatif' height='150px' >";
-    echo "<form  action='gallery.php' method='post'>
-    Nom du fichier <br/> <input type='text' name='pic_name' value={$_FILES['pic']['name']} >
-    Description <br/> <input type='text' name='pic_desc'>
-    Date <br/> <input type='text' name='pic_date'>
+    
+    echo "<form  action='save_pic_db.php' method='post'>
+    
+    <br/> Nom du fichier : <br/> <input type='text' name='pic_name' value={$_FILES['pic']['name']} >
+    <br/> Description : <br/> <input type='text' name='pic_desc'>
+    <br/> Date (DD/MM/YYYY) : <br/> <input type='text' name='pic_date'> <br/>
+    <br/> Kewords (comma separated) : <br/> <input type='text' name='pic_keywords'> <br/>
+    
     <input type='submit' value='Envoyer' >
+    
     </form>";
     
 }
