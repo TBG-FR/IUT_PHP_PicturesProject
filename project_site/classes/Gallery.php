94da6a88b4f422ba59453f715d7f7da49e0bfa5f
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-03 14:07:54 +0100

    Class Gallery : Complete constructor
    Able to construct all different types of galleries (public, private, customized public, admin)

--- project_site/classes/Gallery.php
+++ project_site/classes/Gallery.php
@@ -46,78 +46,134 @@ class Gallery
 
         $bdd_table_user = 'phpproj_user';
         $bdd_table_picture = 'phpproj_picture';
-        $bdd_table_pic_key = 'phpproj_pictureKeyword';
+        $bdd_table_pic_key = 'phpproj_picturekeyword';
         $bdd_table_keyword = 'phpproj_keyword';
         $bdd_table_gallery = 'phpproj_gallery';
-        $bdd_table_gal_pic = 'phpproj_galleryPicture';
+        $bdd_table_gal_pic = 'phpproj_gallerypicture';
 
         /* TEMP TEMP ----- ----- TEMP TEMP */
 
         $db = new Database();
         $this->nb_pictures = 0;
 
+        // IF THE USER IS LOGGED
         if($logged) {
 
-            /*
-            *
-            *   GET THE BOUGHT IMAGES
-            *
-            */
-
+            // IF THIS IS A PUBLIC GALLERY => CONSTRUCT "CUSTOMISED" PUBLIC GALLERY
             if($public) {
 
-                /* CONSTRUCT "CUSTOMISED" PUBLIC GALLERY */
-                $this->id = 1;
+                $this->id = 1; /* A Gallery of id 1 is a Customised Public Gallery */
+
+                // ----------------------- STEP 1 : GET ALL PUBLIC PICTURES
+                $all_public_img = $db->read($bdd_table_picture, array(
+                    'conditions' => array(
+                        'public LIKE' => '1'
+                    ),
+                    'fields' => array('*'),
+                ));
+
+                foreach($all_public_img as $pic) {
+
+                    //  FCT : addPicture($id, $title, $desc, $public, $path, $state)
+                    $this->addPicture($pic['id'], $pic['title'], $pic['description'], $pic['date'], $pic['public'], $pic['path_watermarked'], 0);
+
+                }
+
+                // ----------------------- STEP 2 : GET USER "PRIVATE" (BOUGHT) PICTURES 
+
+                $user_img_list = $db->read($bdd_table_gal_pic, array(
+                    'conditions' => array(
+                        'gal_id LIKE' => $user_id
+                    ),
+                    'fields' => array('pic_id'),
+                ));
+
+                // ----------------------- STEP 3 : EDIT PICTURES STATE (FROM 0 TO 2) ONE BY ONE 
+                foreach($user_img_list as $user_img) {
+
+                    $this->pictures[$user_img['pic_id']]->setState(2);
+
+                }                
 
             }
 
+            // ELSE (NOT A PUBLIC GALLERY) => CONSTRUCT USER PRIVATE GALLERY
             else {
 
-                /* CONSTRUCT USER PRIVATE GALLERY */
                 $this->id = $user_id;
 
-            }
+                // IF THE USER IS THE ADMIN => CONSTRUCT THE "ADMIN" GALLERY
+                if( $user_id == 2 ) {
 
+                    // ----------------------- STEP 1 : GET ALL PICTURES (PUBLIC & NOT)
+                    $all_img = $db->read($bdd_table_picture, array(
+                        'conditions' => array(),
+                        'fields' => array('*'),
+                    ));
+
+                    foreach($all_img as $pic) {
+
+                        //  FCT : addPicture($id, $title, $desc, $public, $path, $state)
+                        $this->addPicture($pic['id'], $pic['title'], $pic['description'], $pic['date'], $pic['public'], $pic['path_original'], 0);
+
+                    }
+
+                }
+
+                //ELSE (NORMAL USER) => CONSTRUCT USER PRIVATE GALLERY
+                else {
+
+                    // ----------------------- STEP 1 : GET USER "PRIVATE" (BOUGHT) PICTURES
+                    $user_img_list = $db->read($bdd_table_gal_pic, array(
+                        'conditions' => array(
+                            'gal_id LIKE' => $user_id
+                        ),
+                        'fields' => array('pic_id'),
+                    ));
+
+                    // ----------------------- STEP 2 : ADD THEM ONE BY ONE IN THE GALLERY
+                    foreach($user_img_list as $user_img) {
+
+                        $pic = $db->read($bdd_table_picture, array(
+                            'conditions' => array(
+                                'id LIKE' => $user_img['pic_id']
+                            ),
+                            'fields' => array('*'),
+                        ));
+
+                        //  FCT : addPicture($id, $title, $desc, $public, $path, $state)
+                        $this->addPicture($pic[0]['id'], $pic[0]['title'], $pic[0]['description'], $pic[0]['date'], $pic[0]['public'], $pic[0]['path_original'], 2);
+
+                    }
+
+                }
+
+            }
 
 
         }
 
+        // ELSE (USER NOT LOGGED) => CONSTRUCT "BASIC" PUBLIC GALLERY
         else {
 
-            /* CONSTRUCT "BASIC" PUBLIC GALLERY */
-            $this->id = 0;
+            $this->id = 0; /* A Gallery of id 0 is the "Basic" Public Gallery */
 
             $all_public_img = $db->read($bdd_table_picture, array(
                 'conditions' => array(
                     'public LIKE' => '1'
                 ),
-                'fields' => array('*'), // get that result (username)
+                'fields' => array('*'),
             ));
-            
-            var_dump($all_public_img);
-            
-            foreach($all_public_img as $pic) {
-                
-                //  FCT : addPicture($id, $title, $desc, $public, $path, $state)
-                $this->addPicture($pic['id'], $pic['title'], $pic['description'], $pic['public'], $pic['path_watermarked'], '0');
-                
-            }
-            
 
-            //            $res = $db->read($bdd_table_gallery, array(
-            //                'conditions' => array(
-            //                    'nom LIKE' => 'H%', // les noms commençant par H
-            //                    'age >' => 20, // les personnes de plus de 20ans
-            //                    'code_postal' => "01000" // qui habitent bourg en bresse
-            //                ),
-            //                'fields' => array('id', 'nom', 'prenom'), // la liste des champs souhaités
-            //                'order' => array('age DESC'), // par age décroissant
-            //                'limit' => 10, // limité à 10 éléments
-            //                'offset' => 5 // à partir du 6eme élément
-            //            ));
+            foreach($all_public_img as $pic) {
+
+                //  FCT : addPicture($id, $title, $desc, $public, $path, $state)
+                $this->addPicture($pic['id'], $pic['title'], $pic['description'], $pic['date'], $pic['public'], $pic['path_watermarked'], 0);
+
+            }
 
         }
-        
+
         return $this;
 
     }
@@ -130,10 +186,9 @@ class Gallery
      * @param _ _
      * @return _ _
      */
-    public function disconnect(){
+    public function getPicture() {
 
-        $this->status = 0;
-
+        //        return $this->status;
     }
 
     /**
@@ -141,9 +196,9 @@ class Gallery
      * @param _ _
      * @return _ _
      */
-    public function getStatus() {
+    public function setXXX() {
 
-        return $this->status;
+        //        return $this->status;
     }
 
     /**
@@ -151,9 +206,9 @@ class Gallery
      * @param _ _
      * @return _ _
      */
-    private function addPicture($id, $title, $desc, $public, $path, $state) {
+    private function addPicture($id, $title, $desc, $date, $public, $path, $state) {
 
-        $this->pictures[$this->nb_pictures+1] = new Picture($id, $title, $desc, $public, $path, $state);
+        $this->pictures[$this->nb_pictures+1] = new Picture($id, $title, $desc, $date, $public, $path, $state);
         $this->nb_pictures = $this->nb_pictures + 1;
 
     }
