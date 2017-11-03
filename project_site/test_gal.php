2f3412e796a6c5d4793cf45f69acdeb76ff4400a
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-03 23:31:53 +0100

    TESTS Gal modif

--- project_site/test_gal.php
+++ project_site/test_gal.php
@@ -17,19 +17,7 @@ require_once("classes/all.inc.php");
         <!-- <meta name="description" content=""> -->
         <!-- <meta name="author" content=""> -->
 
-        <!-- CSS : Bootstrap -->
-        <link href="css/bootstrap.css" rel="stylesheet" media="all" type="text/css">
-
-        <!-- CSS : Fancybox -->        
-        <link  href="css/jquery.fancybox.css" rel="stylesheet">
-
-        <!-- CSS : Custom -->
-        <link href="css/style.css" rel="stylesheet" media="all" type="text/css">
-
-        <!-- Javascript -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="js/bootstrap.js"></script>
-        <script src="js/jquery.fancybox.js"></script>
+        <?php include_once("head.php"); // Make all the CSS & JavaScript links ?>
 
     </head>
 
