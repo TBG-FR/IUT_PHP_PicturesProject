2448559a2801406422b108fdade384e6f7c0c191
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-05 21:53:37 +0100

    PHP Navbar : Cart Items : Bug Fix
    If the cart was empty or not set, an error was displayed
    It's fixed now

--- project_site/navbar_original.php
+++ project_site/navbar_original.php
@@ -1,9 +1,16 @@
+<?php
+
+if( isset($_SESSION['cart']) ) { $cart_items=$_SESSION['cart']->getNbPictures(); }
+else { $cart_items="0"; }
+
+?>
+
 <!-- ------ "Navbar" : Menu de Navigation ------ -->
 <div class="collapse menu">
     <ul class="list-inline">
         <li><a href="index.php">Home</a></li>
         <li><a href="public_gallery.php">Gallery</a></li>
-        <li><a href="cart.php">Cart (<?php echo $_SESSION['cart']->getNbPictures(); ?>)</a></li>
+        <li><a href="cart.php">Cart (<?php echo $cart_items; ?>)</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
 </div>
