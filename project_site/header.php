861127a04b2fc3351f08bbb4e6c7fdacd19541b0
Dyvia <dyviafleury@gmail.com>
2017-11-06 21:08:07 +0100

    challah ca marche HEADER

--- project_site/header.php
+++ project_site/header.php
@@ -1,5 +1,46 @@
-<html>
+<<<<<<< HEAD
+<!-- ------ "Header" : Haut de page (Logo, Contact, Boutons) ------ -->
 
-    <!-- HEADER -->
+<div id="header_head">
+    <div class="lamd3">
+        <img src="images/Logo.png" id="logo" alt="Logo de Ducrozet-Moninot" title="TODO">
+    </div>
 
-</html>
+    <div class="lamd5">
+        <div id="header_contact">
+            Andrew
+            <br/>BLIND 
+        </div>
+    </div>
+
+    <div class="lamd4">
+        <div id="header_buttons">
+            
+        </div>
+    </div>
+</div>
+
+=======
+<!-- ------ "Header" : Haut de page (Logo, Contact, Boutons) ------ -->
+
+<div id="header_head">
+    <div class="lamd3">
+        <img src="images/Logo.png" id="logo" alt="Logo de Ducrozet-Moninot" title="TODO">
+    </div>
+
+    <div class="lamd5">
+        <div id="header_contact">
+            Andrew
+            <br/>BLIND 
+        </div>
+    </div>
+
+    <div class="lamd4">
+        <div id="header_buttons">
+            
+        </div>
+    </div>
+</div>
+
+>>>>>>> ef118fef716784b7c13be286259958cfe189cf44
+<!-- ------ "Header" : Fin du Bloc ------ -->
